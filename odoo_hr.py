89ef47098bef25708a214c8f88039b8f2d0b3878
Doaa <doaanegm1@gmail.com>
2016-06-01 01:04:51 +0200

    insert image in attendance in tree

--- odoo_hr.py
+++ odoo_hr.py
@@ -4,10 +4,27 @@ from openerp import models,fields,api
 class odooHrInhired(models.Model):
     _inherit ="hr.attendance"
     image=fields.Binary()
-    test=fields.Text()
+   # test=fields.Text()
+    @api.onchange('employee_id','image')
     @api.model
     def create(self, values):
-        print values['employee_id']
         contacts1 = self.env['hr.employee'].search([('id','=',values['employee_id'])])
         values['image']=contacts1.image
         return super(odooHrInhired,self).create(values)
+
+
+    #
+    @api.onchange('employee_id')
+    def _onchange_nameEmployee(self):
+        print self
+         # set auto-changing field
+        #self.price = self.amount * self.unit_price
+        # Can optionally return a warning and domains
+        #contacts1 = self.env['hr.employee'].search([('id','=',values['employee_id'])])
+        #values['image']=contacts1.image
+        return {
+            'warning': {
+                'title': "Something bad happened",
+                'message': "It was very bad indeed",
+            }
+        }
