f59df353855f526165dce72c5155ec68cb0d4f8e
mohamed-ashour <e.mohamed77@gmail.com>
2016-06-03 22:07:00 +0200

    adding categories and rules of salary

--- odoo_hr_contarct.py
+++ odoo_hr_contarct.py
@@ -2,4 +2,4 @@ from openerp import models,fields,api
 
 class odooContractInhired(models.Model):
     _inherit ="hr.contract"
-    net=fields.Float(string='Net', required=True)
+    variable=fields.Float(string='Variable', required=True)
