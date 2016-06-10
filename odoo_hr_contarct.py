35ae52d0741ea3e93a12ee5a4f09af6a1ffbe1ae
Eman taha <emeytahaz@gmail.com>
2016-06-10 17:08:27 +0200

    add working hours to contrct

--- odoo_hr_contarct.py
+++ odoo_hr_contarct.py
@@ -3,3 +3,5 @@ from openerp import models, fields, api
 class odooContractInhired(models.Model):
     _inherit ="hr.contract"
     variable=fields.Float(string='Variable', required=True)
+    working_from=fields.Float(string="workig hours from", required=True)
+    working_to=fields.Float(string="workig hours to", required=True)
