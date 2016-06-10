ee6b889c29a6d2871716273f4626938fdac8ffe1
Eman taha <emeytahaz@gmail.com>
2016-06-10 19:58:26 +0200

    add late without store

--- odoo_hr.py
+++ odoo_hr.py
@@ -2,32 +2,8 @@
 
 from openerp import models,fields,api
 
-class odooHrInhired(models.Model):
 
-    _inherit ="hr.attendance"
-    image=fields.Binary()
 
-    #@api.onchange('employee_id','image')
-    @api.model
-    def create(self, values):
-        contacts1 = self.env['hr.employee'].search([('id','=',values['employee_id'])])
-        values['image']=contacts1.image
-        return super(odooHrInhired,self).create(values)
-
-    @api.multi
-    def write(self, values):
-        contacts1 = self.env['hr.employee'].search([('id','=',values['employee_id'])])
-        values['image']=contacts1.image
-        return super(odooHrInhired,self).write(values)
-
-    # @api.onchange('employee_id')
-    # def _onchange_nameEmployee(self):
-    #      return {
-    #         'warning': {
-    #             'title': "Something bad happened",
-    #             'message': "It was very bad indeed",
-    #         }
-    #     }
 
 class odooHrEmployeeInherit(models.Model):
 
