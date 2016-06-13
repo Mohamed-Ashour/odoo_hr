11d88c1805af922f1072c54f31b599ab4fe3d7bd
Eman taha <emeytahaz@gmail.com>
2016-06-13 05:22:57 +0200

    merge late and overtime

--- odoo_hr_attendence.py
+++ odoo_hr_attendence.py
@@ -3,20 +3,19 @@ from openerp import models, fields, api
 class odooAttendenceInherit(models.Model):
     _inherit ="hr.attendance"
 
-    @api.depends('action', 'time', 'start_time')
+    @api.depends('action', 'time', 'start_time', 'end_time')
     def _late_compute(self):
         for record in self:
             if record.action == 'sign_in':
                 record.late = record.time - record.start_time
-        return True
-
-    @api.depends('action', 'time', 'end_time')
-    def _overtime_compute(self):
-        for record in self:
-            if record.action == 'sign_out':
-                record.overtime = record.time - record.end_time - 2
-            if record.overtime < 0:
-                record.overtime = 0
+                if record.late > 0:
+                    record.late = - record.late
+                else:
+                    record.late = 0
+            elif record.action == 'sign_out':
+                record.late = record.time - record.end_time - 2
+                if record.late < 0:
+                    record.late = 0
         return True
 
     @api.model
@@ -41,7 +40,6 @@ class odooAttendenceInherit(models.Model):
     image=fields.Binary()
     time=fields.Float(string="time", required=True)
     late=fields.Float(compute=_late_compute, store=True)
-    overtime=fields.Float(compute=_overtime_compute, store=True)
     start_time=fields.Float(string="start_time")
     end_time=fields.Float(string="end_time")
 
