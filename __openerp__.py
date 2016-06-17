fcd6ad6b8ef5493757a61c9ecbad4d4aa179d54c
Aya Sharaf Elden <ayasharafelden707@gmail.com>
2016-06-17 14:17:01 +0200

    Update National Holidays

--- __openerp__.py
+++ __openerp__.py
@@ -11,6 +11,7 @@ Adding Egyptian rules to HR module
         'odoo_hr_view.xml',
         'odoo_hr_contarct_view.xml',
         'hr_salary_structure_data.xml',
+        'national_holidays_date.xml',
 
         'security/odoo_hr_security.xml',
         'security/ir.model.access.csv',
