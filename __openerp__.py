c25158380f1244f7a82c912b9ce01d453d25819d
Doaa <doaanegm1@gmail.com>
2016-06-18 17:29:49 +0200

    security 2

--- __openerp__.py
+++ __openerp__.py
@@ -22,6 +22,8 @@ Adding Egyptian rules to HR module
         'views/employee_report.xml',
         'views/payslip_report.xml',
         'views/more_details_report.xml',
+        'security/odoo_hr_security.xml',
+        'security/ir.model.access.csv',
 
 
         'odoo_hr_report.xml',
